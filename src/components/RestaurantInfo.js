3eee1b50e3fa8d6f82437eb419bd546dc445c83c
Filip Malek <filamalek@hotmail.com>
2020-03-24 18:17:07 +0100

    added info page, added linting, prettier and lodash

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -1,24 +1,106 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { View, StyleSheet, Text, Image } from 'react-native';
 
-import {View, StyleSheet, Text} from 'react-native';
+// components
+import StarRating from 'components/StarRating';
 
-const RestaurantInfo = ({route, navigation}) => {
-  console.log('navigation', route.params.place)
-  return (
-    <View style={style.main}>
-      <Text style={style.text}>{route.params.place.name}</Text>
-    </View>
-  );
+// utils
+import _get from 'lodash/get';
+import RestaurantImage from 'images/restaurant.png';
+
+const RestaurantInfo = ({ route, navigation }) => {
+	const restaurant = _get(route, 'params.place', null);
+	const name = _get(restaurant, 'name', null);
+	const address = _get(restaurant, 'address', null);
+
+	return (
+		<View style={style.main}>
+
+			{/* restaurant name */}
+			<View style={style.header}>
+				<Text style={style.headerText}>{name}</Text>
+				<View style={style.ratingContainer}>
+					<Text style={style.ratingText}>Rating:</Text>
+					<StarRating entry={restaurant} />
+				</View>
+			</View>
+
+			{/* image and restaurant info (rating, address...) */}
+			<View style={style.content}>
+
+				{/* image wrapper */}
+				<Image
+					style={style.image}
+					source={RestaurantImage} />
+
+				{/* details */}
+				<View style={style.detailsWrapper}>
+					<View style={style.detailsContainer}>
+						<Text style={style.detailsTitle}>Name</Text>
+						<Text style={style.detailsContent}>{name}</Text>
+					</View>
+					<View style={style.detailsContainer}>
+						<Text style={style.detailsTitle}>Address</Text>
+						<Text style={style.detailsContent}>{address}</Text>
+					</View>
+				</View>
+			</View>
+		</View>
+	);
+};
+
+RestaurantInfo.propTypes = {
+	route: PropTypes.object,
+	navigation: PropTypes.object
 };
 
 const style = StyleSheet.create({
-  main: {
-    marginVertical: 50,
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20
-  }
+	main: {
+		flex: 1,
+		padding: 16
+	},
+	header: {
+		flexDirection: 'column',
+	},
+	headerText: {
+		fontSize: 20,
+		marginRight: 10,
+		fontWeight: 'bold'
+	},
+	ratingContainer: {
+		marginTop: 4,
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	ratingText: {
+		marginRight: 4
+	},
+	content: {
+		marginTop: 8,
+		flexDirection: 'row'
+	},
+	image: {
+		height: 150,
+		width: 250,
+		resizeMode: 'contain'
+	},
+	detailsWrapper: {
+		paddingTop: 4,
+		paddingHorizontal: 16,
+		flexDirection: 'column'
+	},
+	detailsContainer: {
+		paddingBottom: 4,
+	},
+	detailsTitle: {
+		fontWeight: '500',
+		color: 'darkgrey'
+	},
+	detailsContent: {
+		fontSize: 15,
+		fontWeight: '600'
+	}
 });
 
 export default RestaurantInfo;
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,36 +1,34 @@
-'use strict';
-
 import React from 'react';
 
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const StarRating = ({entry}) => {
-  const {rating} = entry;
+const StarRating = ({ entry }) => {
+	const { rating } = entry;
 
-  // round to 0.5
-  const roundedEntry = Math.round(rating * 2) / 2;
-  const array = [...Array(Math.ceil(rating))];
+	// round to 0.5
+	const roundedEntry = Math.round(rating * 2) / 2;
+	const array = [...Array(Math.ceil(rating))];
 
-  return (
-    <View style={style.starWrapper}>
-      {array.map((value, index) => {
-        const iconName =
-          index === array.length - 1 && !Number.isInteger(roundedEntry)
-            ? 'star-half'
-            : 'star';
+	return (
+		<View style={style.starWrapper}>
+			{array.map((value, index) => {
+				const iconName =
+					index === array.length - 1 && !Number.isInteger(roundedEntry)
+						? 'star-half'
+						: 'star';
 
-        return <Icon key={index} name={iconName} color="#FFD64C" />;
-      })}
-    </View>
-  );
+				return <Icon key={index} name={iconName} color="#FFD64C" />;
+			})}
+		</View>
+	);
 };
 
 const style = StyleSheet.create({
-  starWrapper: {
-    flexDirection: 'row',
-  },
+	starWrapper: {
+		flexDirection: 'row'
+	}
 });
 
 export default StarRating;
